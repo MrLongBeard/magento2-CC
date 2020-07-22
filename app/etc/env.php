aa0f0f278f972beb4e8319b495243ea175e4e1fe
MrLongBeard <umair.hussain96@gmail.com>
2020-07-22 23:31:55 +0500

    welcome talha

---
+++ app/etc/env.php
@@ -0,0 +1,72 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_cls79v'
+    ],
+    'crypt' => [
+        'key' => '6c45c3020b62a4c6642abef4841ab369'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'mageplaza',
+                'password' => 'YOUR_PASSWORD',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '4cf_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '4cf_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost.com'
+    ],
+    'install' => [
+        'date' => 'Fri, 17 Jul 2020 10:57:58 +0000'
+    ]
+];
